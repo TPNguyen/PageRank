875471b4daaee4d03ee479ee38722b69b03376e4
Cleophus Robinson IV <CleoR41@gmail.com>
2015-02-18 18:58:58 -0800

    Implemented the monte carlo method

--- src/assignment3/MonteCarlo.java
+++ src/assignment3/MonteCarlo.java
@@ -8,26 +8,48 @@ public class MonteCarlo {
 	private ArrayList<Edge> allEdges;
 	private double[] rScores;
 	private int numNodes;
+	private double beta;
+	private Random rand;
 	
-	public MonteCarlo(int numNodes,ArrayList<Edge> allEdges){
+	public MonteCarlo(int numNodes,ArrayList<Edge> allEdges,double beta){
 		nextJump = new HashMap<Integer,ArrayList<Edge>>(numNodes);
 		this.allEdges = new ArrayList<Edge>(allEdges);
 		Collections.sort(this.allEdges);
 		populateMap();
 		this.numNodes = numNodes;
+		this.beta = beta;
 		rScores = new double[numNodes];
+		rand = new Random();
 	}
 	
 	public double[] randomWalk(int RTimes){
+		rScores = new double[rScores.length];
 		for(int i = 0; i < numNodes; i++){
-			for(int r = 0; r < RTimes; r++){
-				if(nextJump.containsKey(i)){
-					ArrayList<Edge> possibilities = nextJump.get(i);
-					//Need to randomly pick one of these
+			if(nextJump.containsKey(i)){
+			ArrayList<Edge> iPossibilities = nextJump.get(i);
+				for(int r = 0; r < RTimes; r++){
+					double termOrContinue = 0.0;
+					int index = 0;
+					ArrayList<Edge> thisPossibilities = new ArrayList<Edge>(iPossibilities);
+					while(termOrContinue <= beta){
+						int choices = thisPossibilities.size();
+						index = rand.nextInt(choices);
+						Edge edge = thisPossibilities.get(index);
+						rScores[edge.to] = rScores[edge.to] + 1;
+						termOrContinue = rand.nextDouble();
+						if(nextJump.containsKey(edge.to)){
+							thisPossibilities = nextJump.get(edge.to);
+						}else{
+							break;
+						}
+					}
 				}
 			}
 		}
-		return null;
+		double num = numNodes*RTimes;
+		double denom = 1 - beta;
+		Matrix.constMultVector(denom/num,rScores);
+		return rScores;
 	}
 	
 	public double[] getRankings(){
@@ -38,7 +60,7 @@ public class MonteCarlo {
 		int size = allEdges.size();
 		for(int i = 0; i < size; i++){
 			Edge edge = allEdges.get(i);
-			if(nextJump.containsKey(edge.from)){
+			if(!nextJump.containsKey(edge.from)){
 				ArrayList<Edge> edgeList = new ArrayList<Edge>();
 				edgeList.add(edge);
 				nextJump.put(edge.from,edgeList);
--- src/assignment3/RunAssignment.java
+++ src/assignment3/RunAssignment.java
@@ -5,16 +5,43 @@ import java.util.*;
 public class RunAssignment {
 
 	public static void main(String[] args) {
+		//The normal page rank
 		PageRank pg = new PageRank("graph.txt",100,.8);
 		ArrayList<Double> rBegin = Matrix.convertToArrayList(pg.getRankings());
-		System.out.println("r begin: " + rBegin);
+		//System.out.println("r begin: " + rBegin);
 		double begin = System.currentTimeMillis();
 		pg.powerIterate(40);
 		double end = System.currentTimeMillis();
-		double time = end - begin;
+		double time = (end - begin)/1000;
 		ArrayList<Double> rEnd = Matrix.convertToArrayList(pg.getRankings());
-		System.out.println("r end: " + rEnd);
-		System.out.println("Time elapsed: " + time);
+		//System.out.println("r end: " + rEnd);
+		System.out.println("Time elapsed pageRank: " + time);
+		//Monte carlo below here
+		ArrayList<Double> monteApprox = null;
+		MonteCarlo mc = new MonteCarlo(100,pg.getEdges(),.8);
+		//For R = 1
+		begin = System.currentTimeMillis();
+		monteApprox = Matrix.convertToArrayList(mc.randomWalk(1));
+		end = System.currentTimeMillis();
+		time = (end - begin)/1000;
+		//System.out.println("mcApprox R1: " + monteApprox);
+		System.out.println("Time elapsed R1: " + time);
+		//For R = 2
+		begin = System.currentTimeMillis();
+		monteApprox = Matrix.convertToArrayList(mc.randomWalk(2));
+		end = System.currentTimeMillis();
+		time = (end - begin)/1000;
+		//System.out.println("mcApprox R2: " + monteApprox);
+		System.out.println("Time elapsed R2: " + time);
+		//For R = 3
+		begin = System.currentTimeMillis();
+		monteApprox = Matrix.convertToArrayList(mc.randomWalk(3));
+		end = System.currentTimeMillis();
+		time = (end - begin)/1000;
+		//System.out.println("mcApprox R3: " + monteApprox);
+		System.out.println("Time elapsed R3: " + time);
 	}
+	
+	
 	
 }
